29f1b89856b1c9c861f495fc8ad24e422713df51
Levir César Ribeiro Lemos <levir.lemos@cagece.com.br>
2021-12-17 17:44:32 -0300

    aula 11 concluida

--- src/test/java/AprenderCucumber.java
+++ src/test/java/AprenderCucumber.java
@@ -1,4 +1,5 @@
-import cucumber.api.PendingException;
+import org.junit.Assert;
+
 import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Então;
 import cucumber.api.java.pt.Quando;
@@ -6,18 +7,40 @@ import cucumber.api.java.pt.Quando;
 public class AprenderCucumber {
 
 	@Dado("^que criei o arquivo corretamente$")
-	public void que_criei_o_arquivo_corretamente() throws Throwable {
-		
+	public void queCrieiOArquivoCorretamente() throws Throwable {
+		System.out.println("qualquer coisa");
 	}
 
 	@Quando("^executá-lo$")
-	public void executá_lo() throws Throwable {
-		
+	public void executáLo() throws Throwable {
+
 	}
 
 	@Então("^a especificação deve finalizar com sucesso$")
-	public void a_especificação_deve_finalizar_com_sucesso() throws Throwable {
-		
+	public void aEspecificaçãoDeveFinalizarComSucesso() throws Throwable {
+
+	}
+
+	@Então("^outro passo qualquer$")
+	public void outroPassoQualquer() throws Throwable {
+
+	}
+
+	private int contador = 0;
+	
+	@Dado("^que o valor do contador é (\\d+)$")
+	public void queOValorDoContadorÉ(int arg1) throws Throwable {
+		contador = arg1;
+	}
+
+	@Quando("^eu incrementar em (\\d+)$")
+	public void euIncrementarEm(int arg1) throws Throwable {
+		contador = contador + arg1;
+	}
+
+	@Então("^o valor do contador será (\\d+)$")
+	public void oValorDoContadorSerá(int arg1) throws Throwable {
+		Assert.assertEquals(arg1, contador);
 	}
 
 }
