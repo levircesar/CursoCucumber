0e0f01fba028d21b3abf74ec9ed76a3134a5bc7f
levircesar <levirlemos@gmail.com>
2021-12-24 13:48:47 -0300

    cenario: alugar filme com sucesso concluido

--- src/test/java/br/ce/wcaquino/runners/Runner.java
+++ src/test/java/br/ce/wcaquino/runners/Runner.java
@@ -7,10 +7,10 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "src/test/resources/features/aprender_cucumber.feature",
+		features = "src/test/resources/features/alugar_filme.feature",
 		glue = "br.ce.wcaquino.steps",
 		plugin = "pretty",
-		tags = "~@ignore",
+		tags = {},
 		monochrome = true,
 		snippets = SnippetType.CAMELCASE,
 		dryRun = false,
