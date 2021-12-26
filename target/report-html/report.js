a22533508a49a141ba3e38faeb9e3e1aa2177efa
levircesar <levirlemos@gmail.com>
2021-12-26 14:43:23 -0300

    contexto concluido

--- target/report-html/report.js
+++ target/report-html/report.js
@@ -13,16 +13,15 @@ formatter.feature({
   "keyword": "Funcionalidade"
 });
 formatter.before({
-  "duration": 155300,
+  "duration": 133300,
   "status": "passed"
 });
-formatter.scenario({
+formatter.background({
   "line": 8,
-  "name": "Deve inserir uma conta com sucesso",
+  "name": "",
   "description": "",
-  "id": "cadastro-de-contas;deve-inserir-uma-conta-com-sucesso",
-  "type": "scenario",
-  "keyword": "Cenário"
+  "type": "background",
+  "keyword": "Contexto"
 });
 formatter.step({
   "line": 9,
@@ -59,26 +58,11 @@ formatter.step({
   "name": "seleciono Adicionar",
   "keyword": "E "
 });
-formatter.step({
-  "line": 16,
-  "name": "informo a conta \"Conta de Teste\"",
-  "keyword": "E "
-});
-formatter.step({
-  "line": 17,
-  "name": "seleciono Salvar",
-  "keyword": "E "
-});
-formatter.step({
-  "line": 18,
-  "name": "a conta é inserida com sucesso",
-  "keyword": "Então "
-});
 formatter.match({
   "location": "InserirContasSteps.queEstouAcessandoAAplicação()"
 });
 formatter.result({
-  "duration": 5674847500,
+  "duration": 4320906900,
   "status": "passed"
 });
 formatter.match({
@@ -91,7 +75,7 @@ formatter.match({
   "location": "InserirContasSteps.informoOUsuário(String)"
 });
 formatter.result({
-  "duration": 83517700,
+  "duration": 54191300,
   "status": "passed"
 });
 formatter.match({
@@ -104,36 +88,59 @@ formatter.match({
   "location": "InserirContasSteps.aSenha(String)"
 });
 formatter.result({
-  "duration": 34274400,
+  "duration": 27785800,
   "status": "passed"
 });
 formatter.match({
   "location": "InserirContasSteps.selecionoEntrar()"
 });
 formatter.result({
-  "duration": 550673100,
+  "duration": 449122200,
   "status": "passed"
 });
 formatter.match({
   "location": "InserirContasSteps.visualizoAPáginaInicial()"
 });
 formatter.result({
-  "duration": 34292100,
+  "duration": 25563800,
   "status": "passed"
 });
 formatter.match({
   "location": "InserirContasSteps.selecionoContas()"
 });
 formatter.result({
-  "duration": 245398500,
+  "duration": 239120900,
   "status": "passed"
 });
 formatter.match({
   "location": "InserirContasSteps.selecionoAdicionar()"
 });
 formatter.result({
-  "duration": 274421500,
+  "duration": 183106500,
   "status": "passed"
+});
+formatter.scenario({
+  "line": 17,
+  "name": "Deve inserir uma conta com sucesso",
+  "description": "",
+  "id": "cadastro-de-contas;deve-inserir-uma-conta-com-sucesso",
+  "type": "scenario",
+  "keyword": "Cenário"
+});
+formatter.step({
+  "line": 18,
+  "name": "informo a conta \"Conta de Teste\"",
+  "keyword": "E "
+});
+formatter.step({
+  "line": 19,
+  "name": "seleciono Salvar",
+  "keyword": "E "
+});
+formatter.step({
+  "line": 20,
+  "name": "a conta é inserida com sucesso",
+  "keyword": "Então "
 });
 formatter.match({
   "arguments": [
@@ -145,33 +152,136 @@ formatter.match({
   "location": "InserirContasSteps.informoAConta(String)"
 });
 formatter.result({
-  "duration": 49403800,
+  "duration": 47198700,
   "status": "passed"
 });
 formatter.match({
   "location": "InserirContasSteps.selecionoSalvar()"
 });
 formatter.result({
-  "duration": 369564700,
+  "duration": 314966600,
   "status": "passed"
 });
 formatter.match({
   "location": "InserirContasSteps.aContaÉInseridaComSucesso()"
 });
 formatter.result({
-  "duration": 24456900,
+  "duration": 52222600,
   "status": "passed"
 });
 formatter.after({
-  "duration": 1092787600,
+  "duration": 874231200,
   "status": "passed"
 });
 formatter.before({
-  "duration": 45300,
+  "duration": 45200,
+  "status": "passed"
+});
+formatter.background({
+  "line": 8,
+  "name": "",
+  "description": "",
+  "type": "background",
+  "keyword": "Contexto"
+});
+formatter.step({
+  "line": 9,
+  "name": "que estou acessando a aplicação",
+  "keyword": "Dado "
+});
+formatter.step({
+  "line": 10,
+  "name": "informo o usuário \"levir@gmail.com\"",
+  "keyword": "Quando "
+});
+formatter.step({
+  "line": 11,
+  "name": "a senha \"levir123\"",
+  "keyword": "E "
+});
+formatter.step({
+  "line": 12,
+  "name": "seleciono entrar",
+  "keyword": "E "
+});
+formatter.step({
+  "line": 13,
+  "name": "visualizo a página inicial",
+  "keyword": "Então "
+});
+formatter.step({
+  "line": 14,
+  "name": "seleciono Contas",
+  "keyword": "Quando "
+});
+formatter.step({
+  "line": 15,
+  "name": "seleciono Adicionar",
+  "keyword": "E "
+});
+formatter.match({
+  "location": "InserirContasSteps.queEstouAcessandoAAplicação()"
+});
+formatter.result({
+  "duration": 4741205000,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "levir@gmail.com",
+      "offset": 19
+    }
+  ],
+  "location": "InserirContasSteps.informoOUsuário(String)"
+});
+formatter.result({
+  "duration": 53137400,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "levir123",
+      "offset": 9
+    }
+  ],
+  "location": "InserirContasSteps.aSenha(String)"
+});
+formatter.result({
+  "duration": 29776300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "InserirContasSteps.selecionoEntrar()"
+});
+formatter.result({
+  "duration": 693574700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "InserirContasSteps.visualizoAPáginaInicial()"
+});
+formatter.result({
+  "duration": 18925200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "InserirContasSteps.selecionoContas()"
+});
+formatter.result({
+  "duration": 245554100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "InserirContasSteps.selecionoAdicionar()"
+});
+formatter.result({
+  "duration": 173704800,
   "status": "passed"
 });
 formatter.scenario({
-  "line": 20,
+  "line": 22,
   "name": "Não deve inserir uma conta sem nome",
   "description": "",
   "id": "cadastro-de-contas;não-deve-inserir-uma-conta-sem-nome",
@@ -179,38 +289,151 @@ formatter.scenario({
   "keyword": "Cenário"
 });
 formatter.step({
-  "line": 21,
-  "name": "que estou acessando a aplicação",
-  "keyword": "Dado "
-});
-formatter.step({
-  "line": 22,
-  "name": "informo o usuário \"levir@gmail.com\"",
-  "keyword": "Quando "
-});
-formatter.step({
   "line": 23,
-  "name": "a senha \"levir123\"",
+  "name": "seleciono Salvar",
   "keyword": "E "
 });
 formatter.step({
   "line": 24,
+  "name": "sou notificado que o nome da conta é obrigatório",
+  "keyword": "Então "
+});
+formatter.match({
+  "location": "InserirContasSteps.selecionoSalvar()"
+});
+formatter.result({
+  "duration": 183101100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "InserirContasSteps.souNotificadoQueONomeDaContaÉObrigatório()"
+});
+formatter.result({
+  "duration": 20104200,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 736336300,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 53300,
+  "status": "passed"
+});
+formatter.background({
+  "line": 8,
+  "name": "",
+  "description": "",
+  "type": "background",
+  "keyword": "Contexto"
+});
+formatter.step({
+  "line": 9,
+  "name": "que estou acessando a aplicação",
+  "keyword": "Dado "
+});
+formatter.step({
+  "line": 10,
+  "name": "informo o usuário \"levir@gmail.com\"",
+  "keyword": "Quando "
+});
+formatter.step({
+  "line": 11,
+  "name": "a senha \"levir123\"",
+  "keyword": "E "
+});
+formatter.step({
+  "line": 12,
   "name": "seleciono entrar",
   "keyword": "E "
 });
 formatter.step({
-  "line": 25,
+  "line": 13,
   "name": "visualizo a página inicial",
   "keyword": "Então "
 });
 formatter.step({
-  "line": 26,
+  "line": 14,
   "name": "seleciono Contas",
   "keyword": "Quando "
 });
 formatter.step({
-  "line": 27,
+  "line": 15,
   "name": "seleciono Adicionar",
+  "keyword": "E "
+});
+formatter.match({
+  "location": "InserirContasSteps.queEstouAcessandoAAplicação()"
+});
+formatter.result({
+  "duration": 4965645600,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "levir@gmail.com",
+      "offset": 19
+    }
+  ],
+  "location": "InserirContasSteps.informoOUsuário(String)"
+});
+formatter.result({
+  "duration": 64078200,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "levir123",
+      "offset": 9
+    }
+  ],
+  "location": "InserirContasSteps.aSenha(String)"
+});
+formatter.result({
+  "duration": 45106500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "InserirContasSteps.selecionoEntrar()"
+});
+formatter.result({
+  "duration": 810563400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "InserirContasSteps.visualizoAPáginaInicial()"
+});
+formatter.result({
+  "duration": 23371600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "InserirContasSteps.selecionoContas()"
+});
+formatter.result({
+  "duration": 245699700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "InserirContasSteps.selecionoAdicionar()"
+});
+formatter.result({
+  "duration": 220929400,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 26,
+  "name": "Não deve inserir uma conta com nome já existente",
+  "description": "",
+  "id": "cadastro-de-contas;não-deve-inserir-uma-conta-com-nome-já-existente",
+  "type": "scenario",
+  "keyword": "Cenário"
+});
+formatter.step({
+  "line": 27,
+  "name": "informo a conta \"Conta mesmo nome\"",
   "keyword": "E "
 });
 formatter.step({
@@ -220,210 +443,8 @@ formatter.step({
 });
 formatter.step({
   "line": 29,
-  "name": "sou notificado que o nome da conta é obrigatório",
-  "keyword": "Então "
-});
-formatter.match({
-  "location": "InserirContasSteps.queEstouAcessandoAAplicação()"
-});
-formatter.result({
-  "duration": 4812479500,
-  "status": "passed"
-});
-formatter.match({
-  "arguments": [
-    {
-      "val": "levir@gmail.com",
-      "offset": 19
-    }
-  ],
-  "location": "InserirContasSteps.informoOUsuário(String)"
-});
-formatter.result({
-  "duration": 75064700,
-  "status": "passed"
-});
-formatter.match({
-  "arguments": [
-    {
-      "val": "levir123",
-      "offset": 9
-    }
-  ],
-  "location": "InserirContasSteps.aSenha(String)"
-});
-formatter.result({
-  "duration": 44298300,
-  "status": "passed"
-});
-formatter.match({
-  "location": "InserirContasSteps.selecionoEntrar()"
-});
-formatter.result({
-  "duration": 512916900,
-  "status": "passed"
-});
-formatter.match({
-  "location": "InserirContasSteps.visualizoAPáginaInicial()"
-});
-formatter.result({
-  "duration": 32746300,
-  "status": "passed"
-});
-formatter.match({
-  "location": "InserirContasSteps.selecionoContas()"
-});
-formatter.result({
-  "duration": 250447500,
-  "status": "passed"
-});
-formatter.match({
-  "location": "InserirContasSteps.selecionoAdicionar()"
-});
-formatter.result({
-  "duration": 213340400,
-  "status": "passed"
-});
-formatter.match({
-  "location": "InserirContasSteps.selecionoSalvar()"
-});
-formatter.result({
-  "duration": 206640300,
-  "status": "passed"
-});
-formatter.match({
-  "location": "InserirContasSteps.souNotificadoQueONomeDaContaÉObrigatório()"
-});
-formatter.result({
-  "duration": 17332600,
-  "status": "passed"
-});
-formatter.after({
-  "duration": 915241600,
-  "status": "passed"
-});
-formatter.before({
-  "duration": 47600,
-  "status": "passed"
-});
-formatter.scenario({
-  "line": 31,
-  "name": "Não deve inserir uma conta com nome já existente",
-  "description": "",
-  "id": "cadastro-de-contas;não-deve-inserir-uma-conta-com-nome-já-existente",
-  "type": "scenario",
-  "keyword": "Cenário"
-});
-formatter.step({
-  "line": 32,
-  "name": "que estou acessando a aplicação",
-  "keyword": "Dado "
-});
-formatter.step({
-  "line": 33,
-  "name": "informo o usuário \"levir@gmail.com\"",
-  "keyword": "Quando "
-});
-formatter.step({
-  "line": 34,
-  "name": "a senha \"levir123\"",
-  "keyword": "E "
-});
-formatter.step({
-  "line": 35,
-  "name": "seleciono entrar",
-  "keyword": "E "
-});
-formatter.step({
-  "line": 36,
-  "name": "visualizo a página inicial",
-  "keyword": "Então "
-});
-formatter.step({
-  "line": 37,
-  "name": "seleciono Contas",
-  "keyword": "Quando "
-});
-formatter.step({
-  "line": 38,
-  "name": "seleciono Adicionar",
-  "keyword": "E "
-});
-formatter.step({
-  "line": 39,
-  "name": "informo a conta \"Conta mesmo nome\"",
-  "keyword": "E "
-});
-formatter.step({
-  "line": 40,
-  "name": "seleciono Salvar",
-  "keyword": "E "
-});
-formatter.step({
-  "line": 41,
   "name": "sou notificado que já existe uma conta com esse nome",
   "keyword": "Então "
-});
-formatter.match({
-  "location": "InserirContasSteps.queEstouAcessandoAAplicação()"
-});
-formatter.result({
-  "duration": 5050415500,
-  "status": "passed"
-});
-formatter.match({
-  "arguments": [
-    {
-      "val": "levir@gmail.com",
-      "offset": 19
-    }
-  ],
-  "location": "InserirContasSteps.informoOUsuário(String)"
-});
-formatter.result({
-  "duration": 74172700,
-  "status": "passed"
-});
-formatter.match({
-  "arguments": [
-    {
-      "val": "levir123",
-      "offset": 9
-    }
-  ],
-  "location": "InserirContasSteps.aSenha(String)"
-});
-formatter.result({
-  "duration": 44477300,
-  "status": "passed"
-});
-formatter.match({
-  "location": "InserirContasSteps.selecionoEntrar()"
-});
-formatter.result({
-  "duration": 532440900,
-  "status": "passed"
-});
-formatter.match({
-  "location": "InserirContasSteps.visualizoAPáginaInicial()"
-});
-formatter.result({
-  "duration": 27472800,
-  "status": "passed"
-});
-formatter.match({
-  "location": "InserirContasSteps.selecionoContas()"
-});
-formatter.result({
-  "duration": 245833100,
-  "status": "passed"
-});
-formatter.match({
-  "location": "InserirContasSteps.selecionoAdicionar()"
-});
-formatter.result({
-  "duration": 236559200,
-  "status": "passed"
 });
 formatter.match({
   "arguments": [
@@ -435,25 +456,25 @@ formatter.match({
   "location": "InserirContasSteps.informoAConta(String)"
 });
 formatter.result({
-  "duration": 57671300,
+  "duration": 36635600,
   "status": "passed"
 });
 formatter.match({
   "location": "InserirContasSteps.selecionoSalvar()"
 });
 formatter.result({
-  "duration": 253145100,
+  "duration": 246339500,
   "status": "passed"
 });
 formatter.match({
   "location": "InserirContasSteps.souNotificadoQueJáExisteUmaContaComEsseNome()"
 });
 formatter.result({
-  "duration": 12719800,
+  "duration": 10432000,
   "status": "passed"
 });
 formatter.after({
-  "duration": 888844100,
+  "duration": 848919400,
   "status": "passed"
 });
 });
